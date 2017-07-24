c3ce64ce15862ee612f1f381e69c08901708e846
in0finite <leshow816@gmail.com>
2017-07-25 01:36:33 +0200

    added example scripts

---
+++ examples/InflictedDamageReporter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace uGameCore {
+
+	/// <summary>
+	/// Detects OnDamaged and OnKilled messages, and informs players about inflicted damage.
+	/// </summary>
+	public class InflictedDamageReporter : MonoBehaviour {
+
+
+		void OnDamaged( InflictedDamageInfo info ) {
+
+			Player attacker = info.player as Player;
+
+			if (attacker != null) {
+				
+				// notify player that he inflicted damage
+
+				//	p.SendMessage ("OnInflictedDamage", info, SendMessageOptions.DontRequireReceiver);
+				attacker.OnInflictedDamage (info.damage);
+
+			}
+
+		}
+
+		void OnKilled( InflictedDamageInfo info ) {
+
+			// notify player that his object is dead
+
+			Player attacker = info.player as Player;
+
+			var deadPlayer = PlayerManager.GetPlayerByGameObject (this.gameObject);
+			if (deadPlayer != null) {
+				deadPlayer.gameObject.BroadcastMessageNoExceptions ("OnDied", attacker);
+			} else {
+				// manually increase num kills
+				if (attacker != null) {
+					attacker.GetComponent<Score.Score>().NumKills++;
+				}
+			}
+
+		}
+
+	}
+
+}
---
+++ examples/Score.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace uGameCore.Score {
+	
+	public class Score : NetworkBehaviour {
+		
+		public	bool	resetOnSceneChange = true;
+
+		[SyncVar(hook="KillScoreChanged")]	private	int numKills = 0;
+		[SyncVar(hook="DeathScoreChanged")]	private	int	numDeaths = 0 ;
+
+		public int NumKills { get { return this.numKills; } set { numKills = value; } }
+		public int NumDeaths { get { return this.numDeaths; } set { numDeaths = value; } }
+
+		private	Player	m_player = null;
+
+
+		// Use this for initialization
+		void Start () {
+
+			m_player = GetComponent<Player> ();
+
+		}
+
+		void OnSceneChanged( SceneChangedInfo info ) {
+
+			if (!this.isServer)
+				return;
+
+			if (this.resetOnSceneChange) {
+				// reset player score
+				this.numKills = 0;
+				this.numDeaths = 0;
+			}
+
+
+		}
+
+		private	void	OnDied( Player playerWhoKilledYou ) {
+
+			if (!this.isServer)
+				return;
+			
+			this.numDeaths++;
+
+			if (playerWhoKilledYou != null) {
+				if (m_player != playerWhoKilledYou) {
+					playerWhoKilledYou.GetComponent<Score>().numKills++;
+				}
+			}
+
+		}
+
+		private	void	KillScoreChanged( int newKills ) {
+
+			if (this.isServer)
+				return;
+			
+			this.numKills = newKills;
+
+		}
+
+		private	void	DeathScoreChanged( int newDeaths ) {
+
+			if (this.isServer)
+				return;
+			
+			this.numDeaths = newDeaths;
+
+		}
+
+
+	}
+
+}
