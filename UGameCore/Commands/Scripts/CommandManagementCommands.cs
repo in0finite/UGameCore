93019f2a5977a5e4f8508e9abfb7a08fc4965e0e
in0finite <leshow816@gmail.com>
2023-08-09 10:11:00 +0200

    add auto-complete for help ; help can display info for a single command ;

--- UGameCore/Commands/Scripts/CommandManagementCommands.cs
+++ UGameCore/Commands/Scripts/CommandManagementCommands.cs
@@ -18,16 +18,60 @@ namespace UGameCore
         }
 
         [CommandMethod("help", allowToRunWithoutServerPermissions = true)]
-        ProcessCommandResult ForbidCmd(ProcessCommandContext context)
+        ProcessCommandResult HelpCmd(ProcessCommandContext context)
         {
-            string response = "List of available commands: " +
-                              string.Join(", ", this.commandManager.RegisteredCommandsDict
-                                  .Select(pair => pair.Key));
+            string response;
+
+            if (context.NumArguments <= 1)
+            {
+                var commands = this.commandManager.RegisteredCommandsDict.ToArray();
+                commands.SortBy(_ => _.Key);
+
+                response = "List of available commands (use `help cmd_name` for details): \n" +
+                              string.Join(", ", commands.Select(_ => _.Key));
+
+                response += "\n\n" + string.Join("\n", commands.Select(_ => _.Key + (_.Value.description != null ? "  -  " + _.Value.description : string.Empty)));
+
+                return ProcessCommandResult.SuccessResponse(response);
+            }
+
+            string cmd = context.ReadString();
+
+            if (!this.commandManager.RegisteredCommandsDict.TryGetValue(cmd, out CommandInfo commandInfo))
+                return ProcessCommandResult.UnknownCommand(cmd);
+
+            response = $"{cmd}";
+            if (commandInfo.description != null)
+                response += "  -  " + commandInfo.description;
+            response += "\n";
+            response += $"requires server perms: {!commandInfo.allowToRunWithoutServerPermissions}   ";
+            response += $"only on server: {commandInfo.runOnlyOnServer}   ";
+            response += $"limit interval: {commandInfo.limitInterval}   ";
+            response += $"auto-complete: {commandInfo.autoCompletionHandler != null}   ";
 
             return ProcessCommandResult.SuccessResponse(response);
         }
 
-        [CommandMethod("command_remove")]
+        [CommandAutoCompletionMethod("help")]
+        ProcessCommandResult HelpCmdAutoComplete(ProcessCommandContext context)
+        {
+            if (context.NumArguments <= 1)
+                return ProcessCommandResult.AutoCompletion(null, null);
+
+            var possibleCompletions = new List<string>();
+
+            //return this.commandManager.AutoCompleteCommand(, out string outExactCompletion, possibleCompletions);
+
+            string cmd = context.ReadString();
+
+            CommandManager.DoAutoCompletion(
+                cmd, this.commandManager.RegisteredCommands, out string outExactCompletion, possibleCompletions);
+
+            if (outExactCompletion != null)
+                outExactCompletion = "help " + outExactCompletion;
+
+            return ProcessCommandResult.AutoCompletion(outExactCompletion, possibleCompletions);
+        }
         ProcessCommandResult RemoveCmd(ProcessCommandContext context)
         {
             string cmd = context.ReadString();
