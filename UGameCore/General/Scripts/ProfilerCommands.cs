0e63a1cbc8fdf92bb3e46559080d53733f3be7fd
in0finite <leshow816@gmail.com>
2023-08-12 01:00:22 +0200

    capture profiler data

--- UGameCore/General/Scripts/ProfilerCommands.cs
+++ UGameCore/General/Scripts/ProfilerCommands.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UGameCore.Utilities;
+using Unity.Profiling;
 using Unity.Profiling.LowLevel.Unsafe;
 using UnityEngine;
 using static UGameCore.CommandManager;
@@ -13,6 +14,9 @@ namespace UGameCore
     {
         public CommandManager commandManager;
 
+        List<(ProfilerRecorder recorder, ProfilerRecorderDescription desc)> m_profilerRecorders = 
+            new List<(ProfilerRecorder, ProfilerRecorderDescription)>();
+
 
         void Start()
         {
@@ -21,11 +25,38 @@ namespace UGameCore
             this.commandManager.RegisterCommandsFromTypeMethods(this);
         }
 
+        void Update()
+        {
+            StringBuilder sb = null;
+            if (m_profilerRecorders.Count > 0)
+                sb = new StringBuilder();
+
+            foreach (var recorder in m_profilerRecorders)
+            {
+                double value = recorder.recorder.CurrentValueAsDouble;
+                if (recorder.recorder.UnitType == ProfilerMarkerDataUnit.Bytes)
+                    value /= (1024 * 1024);
+
+                sb.Append(recorder.desc.Name);
+                sb.Append(" :  ");
+                sb.Append(value);
+                sb.Append("  [");
+                sb.Append(recorder.recorder.UnitType != ProfilerMarkerDataUnit.Bytes ? recorder.recorder.UnitType.ToString() : "MB");
+                sb.Append("]\n");
+
+                recorder.recorder.Stop();
+                recorder.recorder.Dispose();
+            }
+
+            m_profilerRecorders.Clear();
+
+            if (sb != null)
+                Debug.Log(sb.ToString());
+        }
+
         [CommandMethod("profiler_list", "List all available profiler categories")]
         ProcessCommandResult ProfilerListCmd(ProcessCommandContext context)
         {
-            //string categoryInput = context.ReadStringOrDefault(null);
-
             var availableStatHandles = new List<ProfilerRecorderHandle>();
             ProfilerRecorderHandle.GetAvailable(availableStatHandles);
 
@@ -58,8 +89,35 @@ namespace UGameCore
             return ProcessCommandResult.SuccessResponse(sb.ToString());
         }
 
+        [CommandMethod("profiler_category_capture", "Capture all profiler stats in given category")]
+        ProcessCommandResult ProfilerCategoryCaptureCmd(ProcessCommandContext context)
+        {
+            string categoryInput = context.ReadString();
+
+            var availableStatHandles = new List<ProfilerRecorderHandle>();
+            ProfilerRecorderHandle.GetAvailable(availableStatHandles);
+
+            var descs = availableStatHandles
+                .Select(_ => ProfilerRecorderHandle.GetDescription(_))
+                .Where(_ => _.Category.Name.Equals(categoryInput, StringComparison.OrdinalIgnoreCase))
+                .ToArray();
+
+            foreach (ProfilerRecorderDescription desc in descs)
+            {
+                var recorder = new ProfilerRecorder(
+                    new ProfilerMarker(desc.Category, desc.Name),
+                    1,
+                    ProfilerRecorderOptions.StartImmediately | ProfilerRecorderOptions.Default);
+
+                m_profilerRecorders.Add((recorder, desc));
+            }
+
+            return ProcessCommandResult.Success;
+        }
+
         [CommandAutoCompletionMethod("profiler_category")]
-        ProcessCommandResult ProfilerListAutoCompleteCmd(ProcessCommandContext context)
+        [CommandAutoCompletionMethod("profiler_category_capture")]
+        ProcessCommandResult ProfilerCategoryAutoCompleteCmd(ProcessCommandContext context)
         {
             var availableStatHandles = new List<ProfilerRecorderHandle>();
             ProfilerRecorderHandle.GetAvailable(availableStatHandles);
@@ -79,7 +137,7 @@ namespace UGameCore
                 input, categories, out string outExactCompletion, possibleCompletions);
 
             if (outExactCompletion != null)
-                outExactCompletion = "profiler_category " + outExactCompletion;
+                outExactCompletion = context.commandOnly + " " + outExactCompletion;
 
             return ProcessCommandResult.AutoCompletion(outExactCompletion, possibleCompletions);
         }
